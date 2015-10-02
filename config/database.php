01d9fd0f559207433cd5e91c283706f2d9022a77
123581321345589 <vasya.chernenkov@yandex.ru>
2015-10-02 17:29:57 +0300

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+// Имя базы данных
+define('DATABASE_NAME', 'xxxxx');
+ 
+// Имя пользователя
+define('DATABASE_USER', 'xxxxx');
+ 
+// Пароль
+define('DATABASE_PASSWORD', 'xxxxx');
+ 
+// Сервер
+define('DATABASE_HOST', 'localhost');
+ 
+// Кодировка
+define('DATABASE_CHARSET', 'utf8');
+ 
+// Временная зона
+define('DATABASE_TIMEZONE', '+03:00');
